e178f7eabf0f627865ebe41b68f38310f43a4f89
bobbybee <j104@rocketmail.com>
2014-03-12 15:22:22 -0400

    Added dynamic type I/O

--- Packet.js
+++ Packet.js
@@ -15,7 +15,23 @@ Packet.prototype.readInt32 = function(){ this.offset += 4; return this.buf.readI
 Packet.prototype.readInt64 = function(){ this.offset += 8; return this.buf.readUInt32LE(this.offset-8) | (this.buf.readInt32LE(this.offset-4) << 32)};
 Packet.prototype.readBlob = function(l){ var b = new Buffer(l); this.buf.copy(b, 0, this.offset, this.offset+l); this.offset+=l; return b};
 Packet.prototype.readString = function(){ return this.readBlob(this.readUInt16()).toString(); }
-
+Packet.prototype.readType = function(t) {
+    if(t.indexOf("]") && arraySize=t.match(/^([^\[]*)\[([0-9]*)\]$/)) {
+        if(arraySize[2].length) var len = arraySize[2];
+        else len = this.readUInt16();
+        
+        ret = [];
+        while(len--) ret.push(this.readType(arraySize[1]));
+        return ret;    
+    }
+    if(t == "uint8")  return this.readUInt8();
+    if(t == "uint16") return this.readUInt16();
+    if(t == "uint32") return this.readUInt32();
+    if(t == "int8")   return this.readInt8();
+    if(t == "int16")  return this.readInt16();
+    if(t == "int32")  return this.readInt32();
+    if(t == "string") return this.readString();
+}
 function OutPacket(){
     this.buf = [];
 }
@@ -29,8 +45,25 @@ OutPacket.prototype.writeInt32 = function(b){ this.writeUInt32(b); };
 OutPacket.prototype.writeInt64 = function(b){ this.writeUInt64(b); };
 OutPacket.prototype.writeBlob = function(b){ this.writeUInt16(b.length); this.buf = this.buf.concat(b); };
 OutPacket.prototype.writeString = function(str){ this.writeUInt16(str.length); var i = 0; while(i < str.length){ this.buf.push(str[i].charCodeAt(0));++i;}};
+OutPacket.prototype.writeType = function(t,v) {
+    if(t.indexOf("]") && arraySize=t.match(/^([^\[]*)\[([0-9]*)\]$/)) {
+        if(arraySize[2].length) var len = arraySize[2];
+        else {
+            len = v.length;
+            this.writeUInt16(len);
+        }
+        
+        while(len--) this.writeType(arraySize[1]));
+    }
+    if(t == "uint8")  this.writeUInt8(v);
+    if(t == "uint16") this.writeUInt16(v);
+    if(t == "uint32") this.writeUInt32(v);
+    if(t == "int8")   this.writeInt8(v);
+    if(t == "int16")  this.writeInt16(v);
+    if(t == "int32")  this.writeInt32(v);
+    if(t == "string") this.writeString(v);
+}
 
 OutPacket.prototype.serialize = function(){ var l = this.buf.length; return new Buffer([l & 0xFF, (l >> 8) & 0xFF].concat(this.buf));  };
-
 module.exports.Packet = Packet;
 module.exports.OutPacket = OutPacket;
