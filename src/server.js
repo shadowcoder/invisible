a057c57ee3d521ca274c12d3d82555ad17c97d3d
bobbybee <j104@rocketmail.com>
2014-03-13 11:15:02 -0400

    Getters/setters for m_property notation in .classd files

--- src/server.js
+++ src/server.js
@@ -9,12 +9,12 @@ function propByName(class_n, prop) { return iclass[class_n].childArr[iclass[clas
 
 var classd = fs.readFileSync("../virtualground.classd").toString();
 
-var reg = /\s*^[^ ]* ([^:]*)::([^\(]*)\(([^\)]*)[^\n]*([^\}]*)}/g;
+var reg = /\s*[^ ]* ([^:]*)::([^\(]*)\(([^\)]*)[^\n]*([^\}]*)}/g;
 while(res = reg.exec(classd)) {
-var params_t = res[3].split(/,\s*/).join(" ").split(" ");
+    var params_t = res[3].split(/,\s*/).join(" ").split(" ");
     for(var f = 1, params = []; f < params_t.length; f+=2) params.push(params_t[f]);
     propByName(res[1], res[2]).function = eval("(function("+params.join(",")+"){"+res[4].trim().replace(/\n/g, ";")+"})");
-    
+    console.log(res[1]+"::"+res[2]);
 }
 
 var objs = [];
@@ -28,14 +28,36 @@ function netConstruct(class_n) {
     return objs.length - 1;
 }
 
-function ExtensionContext(obj) {
+function ExtensionContext(obj, caller) {
     this.obj = obj;
-}
-ExtensionContext.prototype.set = function(n,v) {
-    this.obj.children[n].value = v;
-}
-ExtensionContext.prototype.read = function(n) {
-    return this.obj.children[n].value;
+    this.caller = caller;
+    
+    var t_con = this;
+    
+    for(var x = 0; x < obj.childArr.length; ++x) {
+        var p = obj.childArr[x];
+        
+        if(p[1].type == "declaration") {
+            (function() { 
+                var p2 = p;
+                console.log(p2);
+                Object.defineProperty(t_con, "m_"+p2[0], {
+                    get: function() {
+                        return p2[1].value;
+                    },
+                    set: function(v) {
+                        p2[1].value = v;
+                        for(var m = 0; m < p2[1].mods.length; ++m) 
+                            if(["public", "publicread", "serverwrite"].indexOf(p2[1].mods[m]) > -1) {
+                                console.log("Broadcasting "+JSON.stringify(p2));
+                            }
+                    }
+                });
+            })();
+        } else if(p[1].type == "method") {
+            this["m_"+p[0]] = p[1].function;
+        }
+    }
 }
 
 var shadowcoder = netConstruct("User", "shadowcoder");
@@ -51,16 +73,19 @@ net.createServer(function(conn) {
             var field = objs[objID].childArr[property];
             console.log(field);
             
-            //TODO: access
+            //TODO: access, zones
             if(field[1].type == 'method') {
                 var params = [];
                 for(var i = 0; i < field[1].params.length; i++) params.push(d.readType(field[1].params[i][1]));
-        
-                propByName(objsTypes[objID], objs[objID].reverseChildren[property]).function.apply(new ExtensionContext(objs[objID]), params);
+                
+                console.log(objsTypes[objID]+"::"+objs[objID].reverseChildren[property])
+                propByName(objsTypes[objID], objs[objID].reverseChildren[property]).function.apply(new ExtensionContext(objs[objID], shadowcoder), params);
         
                 console.log(field[0]+"("+params+")");
             } else if(field[1].type == 'declaration') {
                 field[1].value = d.readType(field[1].datatype);
+                
+                
             }
         }
     });
